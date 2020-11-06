c8d8e5419bc6cb2564275cbf60d0f193239f50e8
fpassow <fpassow@yahoo.com>
2020-11-05 20:59:50 -0500

    Fixed csv formatting

--- src/cannedsoup/app.py
+++ src/cannedsoup/app.py
@@ -4,49 +4,49 @@ Base project for Beautiful Soup scrapers packaged as native apps
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
-
 import requests
 from bs4 import BeautifulSoup
 import csv
-
 import cannedsoup.soupspoon as soupspoon
-
 
 class CannedSoup(toga.App):
 
     def startup(self):
         self.data = []
-        main_box = toga.Box()
-        textbox = toga.MultilineTextInput(id='view1')
+        main_box = toga.Box(style=Pack(direction=COLUMN))
+        textbox = toga.MultilineTextInput(style=Pack(flex=1))
         def get_data(butt):
             page = requests.get(soupspoon.URL)
             soup = BeautifulSoup(page.content, 'html.parser')
             output_rows = soupspoon.extract_rows_from_soup(soup)
             self.data = output_rows
-            textbox.value = str(output_rows)
+            data_string = '';
+            for row in output_rows:
+                data_string += (', '.join(row))
+                data_string += '\n'
+            textbox.value = data_string
         get_data_button = toga.Button('Get Data', on_press=get_data)
 
         def save_data(butt):
             file_name = self.main_window.save_file_dialog('Save Data', 'web_data.csv', file_types=['csv'])
             print(file_name)
-            with open(file_name, 'w') as csvfile:
+            with open(file_name, 'w', newline='') as csvfile:
                 writer = csv.writer(csvfile)
                 writer.writerows(self.data)
         save_data_button = toga.Button("Save Data", on_press=save_data)
+        controls_box = toga.Box(children=[get_data_button, save_data_button], style=Pack(padding=10))
 
-        main_box.add(get_data_button)
-        main_box.add(save_data_button)
+        main_box.add(controls_box)
         main_box.add(textbox)
 
-
-        self.main_window = toga.MainWindow(title=self.formal_name)
+        title_string = 'Set HUMAN_FRIENDLY_NAME in soupspoon.py'
+        if soupspoon.HUMAN_FRIENDLY_NAME:
+            title_string = soupspoon.HUMAN_FRIENDLY_NAME
+        self.main_window = toga.MainWindow(title=title_string)
         self.main_window.content = main_box
         self.main_window.show()
 
 
 def main():
     return CannedSoup()
-
-# Example to scrape https://github.com/fpassow/cannedsoup
-# Look for "Example Header" in a <th></th>
 
--- src/cannedsoup/soupspoon.py
+++ src/cannedsoup/soupspoon.py
@@ -1,13 +1,24 @@
 
 # This is an example.
 # Edit to:
+#   1. Set a HUMAN_FRIENDLY_NAME for the app
 #   1. Insert your own URL
-#   2. Rewrite extract_rows_from_soup(soup) to extrat the data you want in your CSV
+#   2. Rewrite the extract_rows_from_soup(soup) function to extrat the data you want
+#      and return it as a list of lists, which will become the rows in the output csv.
+
+HUMAN_FRIENDLY_NAME = 'Cannedsoup Example Name'
 
 URL = 'https://github.com/fpassow/cannedsoup'
 
 def extract_rows_from_soup(soup):
-    table = soup.find(text='Example Header').parent.parent.parent.parent
+    # soup is a BeautifulSoup object created with the HTML fetched from the above URL.
+    # In this example we look for "Example Header" in a <th></th>.
+    # Then move up to the <table> element, down to the <tbody>, and start processing <tr>'s
+    # Please consult docs or tutorials on the python BeatifulSoup package to help you
+    #    write your own code to extrat the date you want.
+    # The function should then return a list of lists, where the inner lists will become rows
+    #    in the outputted .csv
+    table = soup.find(text='Example Header').parent.parent.parent.parent.find('tbody')
     output_rows = []
     for table_row in table.findAll('tr'):
         columns = table_row.findAll('td')
